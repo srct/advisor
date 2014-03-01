79850e920150e23a34a975bc848a8fbb14ef8f88
Chris Reffett <cmreffett@gmail.com>
2014-03-01 14:41:27 -0500

    Inital models.py

--- advisor/mainapp/models.py
+++ advisor/mainapp/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Program(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    description = models.TextField(blank=True)
+    courses = models.ManyToManyField(MetaCourse)
+
+    class Meta:
+        abstract = True
+
+
+class Major(Program):
+    gened = models.ForeignKey(GenEd)
+    concentration = models.ForeignKey(Concentration, blank=True)
+
+
+class Minor(Program):
+    pass
+
+
+class GenEd(Program):
+    pass
+
+
+class Concentration(Program):
+    pass
+
+
+class MetaCourse(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    catalogyear = models.IntegerField()
+    description = models.TextField(blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Course(MetaCourse):
+    prerequisites = models.ManyToManyField(MetaCourse)
+    corequisites = models.ManyToManyField(MetaCourse, blank=True)
+
+
+class CourseGroup(MetaCourse):
+    courses = models.ManyToManyField(Course)
+    numneeded = models.IntegerField()
