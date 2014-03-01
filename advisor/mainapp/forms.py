77c3f1b1973ca7d3fce4c5093e9dc17ac6673905
Daniel W Bond <dbond2@gmu.edu>
2014-03-01 16:58:03 -0500

    first shot at forms, need models to be updated before continuing

--- advisor/mainapp/forms.py
+++ advisor/mainapp/forms.py
@@ -7,34 +7,62 @@ from mainapp.models import Student, Major, Minor, GenEd
 class NewMajorForm( ModelForm ):
     class Meta:
         model = Major
-        fields = [
+        fields = ['name',
+        # not immediately sure how to take other objects as fields
+        # 'concentration',
         ]
-        exclude = [
+        exclude = ['slug', 'description', 'courses', 'gened',
+        # remove when figure out how
+        'concentration',
         ]
         labels = {
+        'name':'Major',
+        # 'concentration':'Concentration',
         }
         widgets = {
+            'name':forms.TextInput(attrs={
+                'class':'form-control',
+                'placeholder':'Government and International Politics',
+            }),
+            # 'concentration':forms.TextInput(attrs={
+            #    'name':
+        }
+
+class NewMinorForm( ModelForm ):
+        model = Semester
+        fields = ['name', ]
+        exclude = ['slug', 'description', 'courses', ]
+        labels = {
+            'name':'Minor',
+        }
+        widgets = {
+            'name':forms.TextInput(attrs={
+                'class':'form-control',
+                'placeholder':'Software Engineering',
+            }),
         }
 
 # form on build page
 class BuildSemesterForm( ModelForm ):
     class Meta:
         model = Semester
-        fields = [
-        ]
-        exclude = [
-        ]
+        fields = ['courses', ]
+        exclude = ['number', 'user', ]
         labels = {
+            'courses' : 'Possible Courses',
         }
         widgets = {
+            'courses':forms.CheckboxSelectMultiple(attrs={
+                'class':'form-control',
+                'placeholder':'',
+            }),
         }
 
 # form on user page
 class StudentUpdateForm( ModelForm ):
     class Meta:
         model = Student
-        fields = [
-        ]
+        fields = [ ]
         exclude = (
         )
         labels = {
