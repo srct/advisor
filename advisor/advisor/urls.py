bf4003330fab14dcd5d092f84e28ec533bb85b7b
Ben Waters <bsawyerwaters@gmail.com>
2014-03-01 16:36:43 -0500

    OK YEAH WHAT

--- advisor/advisor/urls.py
+++ advisor/advisor/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from mainapp import views
 from django.contrib import admin
 admin.autodiscover()
 
--- advisor/mainapp/views.py
+++ advisor/mainapp/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
-
+from rest_framework import viewsets
+MetaCourse, Course, CourseGroup
+from .serializers import ProgramSerializer, CourseSerializer,
+CourseGroupSerializer, TrajectorySerializer
 from .models import Trajectory, Program, Major, Minor, GenEd, Concentration,
 MetaCourse, Course, CourseGroup
 # Create your views here.
+#API SHIT
+
+class ProgramViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer
+
+class CourseSerializer(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class CourseGroupSerializer(viewsets.ReadOnlyModelViewSet):
+    queryset = CourseGroup.objects.all()
+    serializer_class = CourseGroupSerializer
+
+
+class TrajectorySerializer(viewsets.ModelViewSet):
+    queryset = Trajectory.objects.all()
+    serializer_class = TrajectorySerializer
+
 #Trajectory CBVS
 class CreateTrajectory(CreateView):
     model = Trajectory
