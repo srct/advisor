4ca6df29335b51644024d8cf1b7e7714a5ced36d
Renfred Harper <renfredxh@gmail.com>
2014-03-02 01:59:10 -0500

    Add url settings to backbone models

--- advisor/mainapp/static/js/advisor/models/program.js
+++ advisor/mainapp/static/js/advisor/models/program.js
@@ -1,11 +1,14 @@
 var advisor = advisor || {};
 
 advisor.Program = Backbone.RelationalModel.extend({
+  urlRoot: '/api/programs',
+
   relations: [{
     type: Backbone.HasMany,
     key: 'requirements',
     relatedModel: 'advisor.Requirement',
-    collectionType: 'RequirementCollection',
+    collectionType: 'advisor.RequirementCollection',
+    autoFetch: true,
     reverseRelation: {
       key: 'program',
       includeInJSON: 'id'
