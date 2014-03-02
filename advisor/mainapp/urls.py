c5df47ea16d009b2b8abeaa4a4daf63546e9df06
Renfred Harper <renfredxh@gmail.com>
2014-03-01 20:40:40 -0500

    Create new mock build view

--- advisor/mainapp/urls.py
+++ advisor/mainapp/urls.py
@@ -17,5 +17,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'advisor.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^', include(router.urls)),
+    url(r'^build/', build_trajectory, name='build')
 )
 
