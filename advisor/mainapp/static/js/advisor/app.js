3aef0f2eba615672335c9c3fef3b2c32dd5277c3
Renfred Harper <renfredxh@gmail.com>
2014-03-02 13:23:22 -0500

    Add ok trajectory support

--- advisor/mainapp/static/js/advisor/app.js
+++ advisor/mainapp/static/js/advisor/app.js
@@ -2,6 +2,7 @@ var advisor = advisor || {};
 
 
 $(function () {
+  traj = parseInt($('trajid').text()) || 1;
   advisor.Programs = new advisor.ProgramCollection();
   var p = new advisor.Program({id: 3});
   advisor.Programs.reset([p]);
