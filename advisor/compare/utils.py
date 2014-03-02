145367b28f6fcf5020b01da6486e970fbbbefc97
Daniel W Bond <dbond2@gmu.edu>
2014-03-02 01:41:40 -0500

    skeletal work on compare

---
+++ advisor/compare/utils.py
@@ -0,0 +1,36 @@
+# from compare.html
+
+def longestTrajectory():
+    return True
+
+def highLevelCourses():
+    return True
+
+def lightestCourseload():
+    return True
+
+def commonClasses():
+    return True
+
+def extraCourses():
+    return True
+
+# from analytics.html
+
+def mostPopularCourses():
+    return True
+
+def mostPopularPrograms():
+    return True
+
+def mostPopularDepartment():
+    return True
+
+def BAvBS():
+    return True
+
+def hasMinors():
+    return True
+
+def mostPopularCombinations():
+    return True
