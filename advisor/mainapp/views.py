082abc0f1b446d818e1b9a6349d3556fe1b972c1
Ben Waters <bsawyerwaters@gmail.com>
2014-03-02 09:35:27 -0500

    test

--- advisor/mainapp/views.py
+++ advisor/mainapp/views.py
@@ -22,6 +22,7 @@ def build_trajectory(request):
     #process
     if request.method == "POST":
         form = StartTrajectoryForm(request.POST)
+        print form.field.values
         #majors = form.fie
     return render_to_response('build.html', {
     })
