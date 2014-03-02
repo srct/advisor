7471524122dcdc9812dd37572a36c5c25c476715
Daniel W Bond <dbond2@gmu.edu>
2014-03-01 20:18:38 -0500

    wrote two of the processing algorithms

--- advisor/mainapp/utils.py
+++ advisor/mainapp/utils.py
@@ -1,19 +1,22 @@
 from mainapp.models import Course # import more
 
-# creating a trajectory
+### creating a trajectory
 
-def maxProgramsAllowed():
-    """ the maximum programs allowed, e.g. 2 majors and 3 minors tops """
-    return True
+#def maxProgramsAllowed():
+#    """ the maximum programs allowed, e.g. 2 majors and 3 minors tops """
+#   return True
 
-# automatically building a trajectory
+### automatically building a trajectory
 
-# editing a trajectory
+#def someautomaticfunction():
+
+### editing a trajectory
 
 def requirementFulfilled():
-    """ yikes! I don't know how to do this, but I need to set whether
-        requirements are already fulfilled... this is a HUGE issue """
+    """ is this handled in the models? if a requirement if fulfilled, then don't look at thoses classes again """
     return True
+
+# add to a list of requirements filled
 
 def remainingReqCourses():
     """ returns the remaining required courses given previous courses taken """
@@ -25,27 +28,46 @@ def previousCourses():
     # it would unfortunately be really hard for this to be saved somewhere
     return True
 
-def nextCourses():
+def nextCourses(remainingReqCourses, previousCourses):
     """ which courses you can take next based on prereqs and coreqs, given
         previous courses and remaining required courses """
-    return True
+    nextCourses = []
+    for course in remainingReqCourses:
+        reqs = set()
+        course.prereq.append(reqs)
+        course.coreq.append(reqs)
+        for req in reqs:
+            if req in previousCourses:
+                nextCourses.append(course)
 
-def maxCreditsAllowed():
-    """ the maximum credits allowed for a semester-- returns different values
-        to warn or disallow if maximum credits are touched or exceeded """
+    return nextCourses
 
-def findDependancies():
+# IMPLEMENT THIS IN JS!
+#def maxCreditsAllowed():
+#    """ the maximum credits allowed for a semester-- returns different values
+#        to warn or disallow if maximum credits are touched or exceeded """
+
+def findDependancies(deletedCourse, semester):
     """ if a student removes a course while editing, find all courses
         that have require the removed courses """
-    return True
+    foundDependancies = []
+    for suspectCourse in semester.nextSemester.courses:
+        reqs = set()
+        suspectCourse.prereq.append(reqs)
+        suspectCourse.coreq.append(reqs)
+            for req in reqs:
+                if req is deletedCourse:
+                    req.append(foundDependancies)
+                    findDependancies(deletedCourse, semester.nextSemester)
+
+    return foundDependancies
 
 def enoughCredits():
     """ checks if enough credits have been taken to graduate """
     return True
 
-# student page
+### student page
 
 def trajectoryEnds():
     """ returns all paths to each end node of the trajectories tree """
     return True
-    
