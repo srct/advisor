8abfe69187a316d2c0e7b6c1723cfe7bab7fe238
Ben Waters <bsawyerwaters@gmail.com>
2014-03-01 21:36:11 -0500

    fixed urls AGAIN MY BAD

--- advisor/mainapp/urls.py
+++ advisor/mainapp/urls.py
@@ -3,7 +3,7 @@ from mainapp.views import (ProgramViewSet, CourseViewSet, CourseGroupViewSet,
 TrajectoryViewSet, build_trajectory, SemesterViewSet, BuildResponseViewSet,
 DetailStudent)
 from mainapp.models import (Program, Course, CourseGroup, Trajectory, Semester,
-BuildResponse)
+BuildResponse, Student)
 
 from rest_framework.routers import DefaultRouter
 
@@ -20,8 +20,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'advisor.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^', include(router.urls)),
-    url(r'^build/', build_trajectory, name='build')
+    url(r'^build/', build_trajectory, name='build'),
     url(r'^student/(?<pk>[0-9]+)/', DetailStudent.as_view(),
-    name='detail-student')
+    name='detail-student'),
 )
 
