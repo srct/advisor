89f92545edb17d26f9b5ea5ba6eaf64be8f7d7f3
Ben Waters <bsawyerwaters@gmail.com>
2014-03-02 10:10:16 -0500

    fuck you forms

--- advisor/mainapp/views.py
+++ advisor/mainapp/views.py
@@ -22,7 +22,7 @@ def build_trajectory(request):
     #process
     if request.method == "POST":
         form = StartTrajectoryForm(request.POST)
-        majors = form.fields['majors']
+        majors = form.fields['majors'].values()
         print majors
         #majors = form.fie
     return render_to_response('build.html', {
